36c624e1255a855055e412234f5a15285ed40e26
mastalier1997 <jan.mastalier@gmail.con>
2020-01-01 16:23:42 +0100

    server

--- src/main/java/com/example/servingwebcontent/WebController.java
+++ src/main/java/com/example/servingwebcontent/WebController.java
@@ -1,17 +1,13 @@
 package com.example.servingwebcontent;
 
-import org.apache.commons.collections4.MultiValuedMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
-import javax.validation.Valid;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class WebController {
@@ -32,7 +28,7 @@ public class WebController {
         return "handlebar";
     }
 
-    String s=null;
+    public String type=null;
     @RequestMapping("/schaltung")
     public String schaltung(@RequestParam(name="name", required=false) String name, @RequestParam String testOrder, Model model) throws IOException {
         ProductsWeb productsWeb = new ProductsWeb();
@@ -40,30 +36,38 @@ public class WebController {
         list=productsWeb.schaltung(testOrder);
         model.addAttribute("gear", list);
         model.addAttribute("testOrder", testOrder);
-        s=testOrder;
+        type=testOrder;
         return "schaltung";
     }
 
-    String s1=null;
+    public String material=null;
     @RequestMapping("/griff")
     public String griff(@RequestParam(name="name", required=false) String name, @RequestParam String testOrder,Model model) throws IOException {
         ProductsWeb productsWeb = new ProductsWeb();
         List<String> list = new ArrayList<String>();
         list=productsWeb.griff(testOrder);
         model.addAttribute("handle", list);
-        s1=testOrder;
+        material=testOrder;
         return "griff";
     }
 
-    String s2=null;
+    public String gear=null;
     @RequestMapping("/material")
     public String material(@RequestParam(name="name", required=false) String name, @RequestParam String testOrder, Model model) throws IOException {
         ProductsWeb productsWeb = new ProductsWeb();
         List<String> list = new ArrayList<String>();
-        list=productsWeb.material(s);
+        list=productsWeb.material(type);
         model.addAttribute("material", list);
-        s2=testOrder;
+        gear=testOrder;
         return "material";
+    }
+
+    public String handle=null;
+    @RequestMapping("/overview")
+    public String overview(@RequestParam(name="name", required=false) String name,@RequestParam String testOrder, Model model){
+
+        handle=testOrder;
+        return "overview";
     }
 
 
