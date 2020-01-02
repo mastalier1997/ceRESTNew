5a089054e1f8340aa11ec89ca380eb6930e489dc
mastalier1997 <jan.mastalier@gmail.con>
2020-01-02 15:36:29 +0100

    server

--- src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -2,9 +2,15 @@ package com.example.servingwebcontent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ServingWebContentApplication {
+public class ServingWebContentApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder applicationBuilder){
+        return applicationBuilder.sources(ServingWebContentApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ServingWebContentApplication.class, args);
