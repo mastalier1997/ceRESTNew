1155033d9154213bc43cfd91f1240097ba63e1f1
mahsum97 <mahsum.yildirim97@gmail.com>
2020-01-02 15:43:31 +0100

    UserDone

--- src/main/java/com/example/servingwebcontent/Overview.java
+++ src/main/java/com/example/servingwebcontent/Overview.java
@@ -1,5 +1,42 @@
 package com.example.servingwebcontent;
 
 public class Overview {
+    String handlebar;
+    String gear;
+    String material;
+    String grip;
+
+    public void setGrip(String grip) {
+        this.grip = grip;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public void setGear(String gear) {
+        this.gear = gear;
+    }
+
+    public void setHandlebar(String handlebar) {
+        this.handlebar = handlebar;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getGrip() {
+        return grip;
+    }
+
+    public String getGear() {
+        return gear;
+    }
+
+    public String getHandlebar() {
+        return handlebar;
+    }
+
 
 }
